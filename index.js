6f807bd06bfdf178b7b4cfe06d3da58decac09ce
ayodhya1411 <161133222+ayodhya1411@users.noreply.github.com>
2024-03-21 18:29:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,361 @@
+document.querySelector(".btn1").addEventListener("click",function(){
+   alert("I got click play");
+   var btn = new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn2").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn3").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn4").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn5").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn6").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn7").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("")
+   btn.play();
+})
+document.querySelector(".btn8").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn9").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("illahi")
+   btn.play();
+})
+document.querySelector(".btn10").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("illhi")
+   btn.pause();
+})
+document.querySelector(".btn11").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kaun tuze")
+   btn.play();
+})
+document.querySelector(".btn12").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kaun tuze")
+   btn.pause();
+})
+document.querySelector(".btn13").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kaun tuze")
+   btn.play();
+})
+document.querySelector(".btn14").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kaun tuze")
+   btn.pause();
+})
+document.querySelector(".btn15").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("")
+   btn.play();
+})
+document.querySelector(".btn16").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("tum hi aana")
+   btn.pause();
+})
+document.querySelector(".btn17").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("tum hi aana")
+   btn.play();
+})
+document.querySelector(".btn18").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kaabil mp3")
+   btn.pause();
+})
+document.querySelector(".btn19").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("chaleyaa .mp3")
+   btn.play();
+})
+document.querySelector(".btn20").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("chaleyaa .mp3")
+   btn.pause();
+})
+document.querySelector(".btn21").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn22").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn23").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("bairiyaa .mp3")
+   btn.play();
+})
+document.querySelector(".btn24").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("bairiyaa .mp3")
+   btn.pause();
+})
+document.querySelector(".btn25").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("duniya .mp3")
+   btn.play();
+})
+document.querySelector(".btn26").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("duniya .mp3")
+   btn.pause();
+})
+document.querySelector(".btn27").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("besabriyaa.mp3")
+   btn.play();
+})
+document.querySelector(".btn28").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("besabriyaa.mp3")
+   btn.pause();
+})
+document.querySelector(".btn29").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn30").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn31").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn32").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn33").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn34").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn35").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn36").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn37").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn38").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn39").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn40").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn41").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn42").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn43").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn44").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn45").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn46").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn47").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn48").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn49").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn50").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn51").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn52").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn53").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn54").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn55").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn56").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn57").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn58").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn59").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn60").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn61").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn62").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn63").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn64").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn65").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn66").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn57").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn68").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn69").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn70").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+document.querySelector(".btn71").addEventListener("click",function(){
+   alert("I got click play");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.play();
+})
+document.querySelector(".btn72").addEventListener("click",function(){
+   alert("I got click pause");
+   var btn =new Audio("kabira.mp3.mp3")
+   btn.pause();
+})
+
